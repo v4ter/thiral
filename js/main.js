104146f209f876e989abb22256fc7c741354606a
vater <v4ter@hotmail.com>
2012-06-21 10:09:19 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,90 @@
+$(function(){
+	$(window).scrollTop(0);
+	var mode = 'scrolling';
+
+	$('a').on('click', function(e){
+		e.preventDefault();
+		mode = 'anchoring';
+
+		var id = $(this).attr('href');
+		$('.nav a').removeClass('active');
+		$('nav a[href=' + id + ']').addClass('active visited');
+
+		var section = $(this).attr('href');
+
+		if(!$(section).hasClass('open')){
+			var curIndex = $('.gallery-section').index($('.active')),
+				newIndex = $('.gallery-section').index($(section));
+
+			if(curIndex < newIndex){
+				$(section).children('.gallery').fadeIn(100, function(){
+					$('body').scrollTo(section, 500, {
+						onAfter : function(){
+							$('.gallery-section').removeClass('active');
+							$(section).addClass('active');
+							mode = 'scrolling';
+						}
+					});
+				});
+			} else {
+				$('body').scrollTo(section, 500, {
+					onAfter : function(){
+						$(section).children('.gallery').fadeIn(800, function(){
+							$('.gallery-section').removeClass('active');
+							$(section).addClass('active open');
+							mode = 'scrolling';
+						});
+					}
+				});
+			}
+		} else {
+			$('body').scrollTo(section, 500, {
+				onAfter : function(){
+					$('.gallery-section').removeClass('active');
+					$(section).addClass('active');
+					mode = 'scrolling';
+				}
+			});
+		}
+		
+	});
+
+	$(window).on('scroll', function(){
+		if(mode === 'scrolling' && $('.active').next().length !== 0){
+			var winOffset = $(this).scrollTop(),
+				nextOffset = $('.active').next().offset().top - $(window).height() + 100;
+
+			if(winOffset > nextOffset){
+				var section = $('.active').next(),
+					id = '#' + section.attr('id');
+					newHeight = $(section).height() + $(section).children('.gallery').height() + $(section).children('h2').height();
+					console.log(newHeight);
+
+				$(section).fadeIn(300, function(){
+					$(this).children('.gallery').fadeIn(800);
+					$('.gallery-section').removeClass('active');
+					$(section).addClass('active open');
+
+					$('.nav a').removeClass('active');
+					$('.nav a[href=' + id + ']').addClass('active visited');
+				});
+
+			}
+		}
+	});
+
+	//sticky sidebar
+	var stickyTop = $('.sticky').offset().top; // returns number
+ 
+	$(window).scroll(function(){ // scroll event
+		var windowTop = $(window).scrollTop(); // returns number
+
+		if (stickyTop < windowTop) {
+			$('.sticky').css({ position: 'fixed', top: 10 });
+		}
+		else {
+			$('.sticky').css('position', 'static');
+		}
+
+	});
+});

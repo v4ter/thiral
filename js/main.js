efc9b7f48613b9ba00da01b3475231fbe17eac94
vater <v4ter@hotmail.com>
2012-06-26 18:50:28 +0300

    upgraded - check for bugs

--- js/main.js
+++ js/main.js
@@ -1,89 +1,184 @@
 $(function(){
-	$(window).scrollTop(0);
-	var mode = 'scrolling';
-	$('.active').next().addClass('next');
+	$.ajax({
+		url : 'products.json',
+		success : function(data){
+			app(data);
+		}
+	});
+	
+	function app(data){
+		var navLinkTemplate = _.template($('#nav-link').html()),
+			gallerySectionTemplate = _.template($('#gallery-section').html()),
+			galleryItemTemplate = _.template($('#gallery-item').html()),
+			navDom = $('.nav ul'),
+			mainDom = $('.main');
+			obj = data;
 
-	$('a').on('click', function(e){
-		e.preventDefault();
-		mode = 'anchoring';
+		templating(obj);
 
-		var id = $(this).attr('href');
-		$('.nav a').removeClass('active');
-		$('nav a[href=' + id + ']').addClass('active visited');
+		function templating(obj){
+			_.each(obj, function(properties, seriesId){
+				navDom.append(navLinkTemplate({
+					'title' : properties.title,
+					'id' : seriesId
+				}));
 
-		var section = $(this).attr('href');
+				mainDom.append(gallerySectionTemplate({
+					'title' : properties.title,
+					'id' : seriesId
+				}));
+			});
+		}
 
-		if(!$(section).hasClass('open')){
-			var curIndex = $('.gallery-section').index($('.active')),
-				newIndex = $('.gallery-section').index($(section));
+		function loadImages(sectionDom, callback){
+			var	id = sectionDom.attr('id'),
+				gallery = sectionDom.children('.gallery');
 
-			if(curIndex < newIndex){
-				$(section).children('.gallery').fadeIn(100, function(){
-					$('body').scrollTo(section, 500, {
+			_.each(obj[id]['products'], function(product){
+				gallery.append(galleryItemTemplate({
+					'srcThumb' : product.srcThumb
+				}));
+			});
+
+			if(callback) callback();
+		}
+			
+
+		//on click
+		$('a').on('click', function(e){
+			e.preventDefault();
+			mode = 'anchoring';
+
+			var id = $(this).attr('href'),
+				sectionDom = $(id);
+			$('.nav a').removeClass('active');
+			$('nav a[href=' + id + ']').addClass('active visited');
+
+			if(!sectionDom.hasClass('open')){
+				var curIndex = $('.gallery-section').index($('.active')),
+					newIndex = $('.gallery-section').index(sectionDom);
+
+				if(curIndex < newIndex){
+					for(var i = curIndex; i <= newIndex; i++){
+						$('.gallery-section').eq(i).addClass('title');
+					}
+
+					var callback = function(){
+						sectionDom.children('.gallery').fadeIn(250, function(){
+							$('body').scrollTo(id, 500, {
+								onAfter : function(){
+									
+
+									
+										$('.gallery-section').removeClass('active');
+										sectionDom.addClass('open active').next().addClass('title');
+										mode = 'scrolling';
+								}
+							});
+						});
+					};
+
+					loadImages(sectionDom, callback);
+				} else {
+					$('body').scrollTo(id, 500, {
 						onAfter : function(){
-							$('.gallery-section').removeClass('active');
-							$(section).addClass('active').next().addClass('next');
-							mode = 'scrolling';
+							loadImages(sectionDom, callback);
+
+							function callback(){
+								sectionDom.children('.gallery').fadeIn(800, function(){
+									sectionDom.addClass('open').next().addClass('title');
+									mode = 'scrolling';
+								});
+							}
 						}
 					});
-				});
+				}
 			} else {
-				$('body').scrollTo(section, 500, {
+				$('body').scrollTo(id, 500, {
 					onAfter : function(){
-						$(section).children('.gallery').fadeIn(800, function(){
-							$('.gallery-section').removeClass('active');
-							$(section).addClass('active open').next().addClass('next');
-							mode = 'scrolling';
-						});
+						$('.gallery-section').removeClass('active');
+						sectionDom.addClass('active').next().addClass('title');
+						mode = 'scrolling';
 					}
 				});
 			}
-		} else {
-			$('body').scrollTo(section, 500, {
-				onAfter : function(){
-					$('.gallery-section').removeClass('active');
-					$(section).addClass('active').next().addClass('next');
-					mode = 'scrolling';
-				}
-			});
-		}
+			
+		});
 		
-	});
+		//on scroll
+		$(window).on('scroll', function(){
+			var windowTop = $(this).scrollTop();
 
-	$(window).on('scroll', function(){
-		if(mode === 'scrolling' && $('.active').next().length !== 0){
-			var winOffset = $(this).scrollTop(),
-				nextOffset = $('.active').next().offset().top - $(window).height() + 100;
+			if(mode === 'scrolling' && $('.active').next().length !== 0){
+				var	nextOffset = $('.active').next().offset().top - $(window).height() + 80;
 
-			if(winOffset > nextOffset){
-				var section = $('.active').next(),
-					id = '#' + section.attr('id');
+				if(windowTop > nextOffset){
+					var sectionDom = $('.active').next(),
+						id = '#' + sectionDom.attr('id');
 
-				$(section).fadeIn(300, function(){
-					$(this).children('.gallery').fadeIn(800);
-					$('.gallery-section').removeClass('active');
-					$(section).addClass('active open').next().addClass('next');
+					var callback = function(){
+						$('.gallery-section').removeClass('active');
+						sectionDom.addClass('active open').next().addClass('title');
 
-					$('.nav a').removeClass('active');
-					$('.nav a[href=' + id + ']').addClass('active visited');
-				});
+						$('.nav a').removeClass('active');
+						$('.nav a[href=' + id + ']').addClass('active visited');
+					};
 
+					if(!$('.active').next().hasClass('open')){
+						sectionDom.children('.gallery').fadeIn(800, function(){
+							loadImages(sectionDom, callback);
+						});
+					} else {
+						callback();
+					}
+				}
 			}
-		}
-	});
 
-	//sticky sidebar
-	var stickyTop = $('.sticky').offset().top; // returns number
- 
-	$(window).scroll(function(){ // scroll event
-		var windowTop = $(window).scrollTop(); // returns number
+			//sticky sidebar
+			if (stickyTop < windowTop) {
+				$('.sticky').css({ position: 'fixed', top: 10 });
+			}
+			else {
+				$('.sticky').css('position', 'static');
+			}
+		});
 
-		if (stickyTop < windowTop) {
-			$('.sticky').css({ position: 'fixed', top: 10 });
-		}
-		else {
-			$('.sticky').css('position', 'static');
-		}
+		//initial.setup
+		$('.nav a').first().addClass('active visited');
+		$('.gallery-section').first().addClass('active open');
+		$('.gallery-section.active').next().addClass('title open');
 
-	});
+		var totalHeight = 0;
+		
+		loadImages($('.gallery-section.active'));
+
+		loadImages($('.gallery-section.open').last());
+
+
+		// function checkHeight(h){
+		// 	if(h < $(window).height()){
+		// 		var nextGallery = $('.gallery-section').not('.open').first();
+
+		// 		console.log(nextGallery);
+
+		// 		nextGallery.addClass('open');
+
+		// 		loadImages(nextGallery, function(){
+		// 			console.log(totalHeight);
+		// 			totalHeight += $('.gallery-section.open').last().height();
+		// 			checkHeight(totalHeight);
+		// 		});
+				
+		// 	}
+		// }
+
+		
+
+		var	stickyTop = $('.sticky').offset().top, mode;
+		$(window).scrollTo(0, 0, {
+			onAfter : function(){
+				mode = 'scrolling';
+			}
+		});
+	}
 });
--- js/templating.js
+++ js/templating.js
@@ -1,19 +1,4 @@
-var itemsCount = parseInt($('.container').attr('data-items'), 10),
-	photosCount = parseInt($('.container').attr('data-photos'), 10),
-	navLink = _.template($('#nav-link').html()),
-	gallerySection = _.template($('#gallery-section').html()),
-	galleryItem = _.template($('#gallery-item').html()),
-	navDom = $('.nav ul'),
-	mainDom = $('.main');
 
-for(var i = 0; i < itemsCount; i++){
-	var active = i === 0 ? true : false;
 
-	navDom.append(navLink({'i' : i, 'active' : active}));
-	mainDom.append(gallerySection({'i' : i, 'active' : active}));
 
-	for(var j = 0; j < photosCount; j++){
-		$('.gallery-section > .gallery').eq(i).append(galleryItem({}));
-	}
-}
 
